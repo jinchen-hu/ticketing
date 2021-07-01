873d079b2a48993b84f971ae796323e92ad1e909
jinchen <jinchenhu.luke@gmail.com>
2021-07-01 00:37:02 -0400

    test for currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { currentUser } from "../middlewares/current-user";
+import { StatusCodes } from "http-status-codes";
 
 const router = express.Router();
 
