66691a266f2721a154a417d963bbaf39d90913f6
jinchen <jinchenhu.luke@gmail.com>
2021-07-01 00:27:15 -0400

    add status code

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const router = express.Router();
 
 router.post("/api/users/signout", (req: Request, res: Response) => {
   req.session = null;
 
-  res.send({});
+  res.status(StatusCodes.OK).send({});
 });
 
 export { router as signoutRouter };
