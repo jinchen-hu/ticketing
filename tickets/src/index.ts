622d89f6373deb00a86de120c46956e1ed885de1
jinchen <jinchenhu.luke@gmail.com>
2021-07-04 18:21:49 -0400

    add nats client env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,11 +12,23 @@ const start = async () => {
     throw new Error("MONGO_URL must be defined");
   }
 
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
   try {
     await natsWrapper.connect(
-      "ticketing",
-      "abbbb",
-      "http://ticketing-nats-srv:4222"
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
     );
 
     natsWrapper.client.on("close", () => {
