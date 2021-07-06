b9667477f898eed4aa5149c262ad8567f18959eb
jinchen <jinchenhu.luke@gmail.com>
2021-07-06 02:38:02 -0400

    add version number to events

--- common/build/events/order-cancelled-event.d.ts
+++ common/build/events/order-cancelled-event.d.ts
@@ -3,6 +3,7 @@ export interface OrderCancelledEvent {
     subject: Subjects.ORDER_CANCELLED;
     data: {
         id: string;
+        version: number;
         ticket: {
             id: string;
         };
--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -1,11 +1,12 @@
 import { Subjects } from "./subjects";
 
-export interface TicketUpdatedEvent{
+export interface TicketUpdatedEvent {
   subject: Subjects.TICKET_UPDATED;
   data: {
     id: string;
     title: string;
     price: number;
     userId: string;
+    version: number;
   };
 }
