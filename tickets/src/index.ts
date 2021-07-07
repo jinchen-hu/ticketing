390beb5bf9b3194040139d3f9b3662a583b68dc9
jinchen <jinchenhu.luke@gmail.com>
2021-07-07 05:21:49 -0400

    add listen

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -41,8 +41,8 @@ const start = async () => {
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
 
-    new OrderCreatedListener(natsWrapper.client);
-    new OrderCancelledListener(natsWrapper.client);
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
