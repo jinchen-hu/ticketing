67cbba077877105ebe100c709498ca86e115e76c
jinchen <jinchenhu.luke@gmail.com>
2021-07-08 19:49:56 -0400

    add show order page

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,50 @@
+import { BaseLayout } from "../../components/BaseLayout";
+import { buildClient } from "../../api/build-client";
+import { useEffect, useState } from "react";
+
+const OrderShow = ({ currentUser, order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      setTimeLeft(Math.round((new Date(order.expiresAt) - new Date()) / 1000));
+    };
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  const timerDisplay =
+    timeLeft <= 0 ? "OrderExpired" : `Time left to pay: ${timeLeft} seconds`;
+
+  return <BaseLayout currentUser={currentUser}>{timerDisplay}</BaseLayout>;
+};
+
+export default OrderShow;
+
+export async function getServerSideProps(context) {
+  const client = buildClient(context);
+
+  let currentUser, order;
+  try {
+    const currentUserRes = await client.get("/api/users/currentuser");
+    currentUser = currentUserRes?.data?.currentUser || null;
+
+    const { orderId } = context.query;
+    console.log("current user loaded in show order page", currentUser);
+
+    const orderRes = await client.get(`/api/orders/${orderId}`);
+    order = orderRes?.data || null;
+    console.log("order loaded in show order page", order);
+  } catch (e) {
+    console.log(
+      "something wrong when fetching data from client in show order page"
+    );
+  }
+
+  return {
+    props: { currentUser, order },
+  };
+}
